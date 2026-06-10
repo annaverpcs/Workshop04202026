bed183c4ed02dc53964563e2c36e87ee99971edb
yulia-zova <yulia.geyko@gmail.com>
2026-06-10 19:54:12 -0400

    Created Step Definition

--- src/test/java/definitions/YuliaKStepDefs.java
+++ src/test/java/definitions/YuliaKStepDefs.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 import static support.TestContext.getDriver;
 
 
@@ -21,5 +24,18 @@ public class YuliaKStepDefs {
     @And("element with text {string} should be present")
     public void findText(String text) {
         getDriver().findElement(By.xpath("//*[text()='" + text + "']"));
+    }
+
+    @And("element with text {string} should not be present")
+    public void findTextNotPresent(String text) {
+        List<WebElement> elements = getDriver().findElements(By.xpath("//*[text()='" + text + "']"));
+        assertTrue(elements.isEmpty());
+    }
+
+//    Then I click on element with xpath "//button[contains(text(),'09:30 AM')]"
+    @When("I click on element with text {string}")
+    public void clickOnElementByText(String text) {
+        WebElement element = getDriver().findElement(By.xpath("//*[text()='" + text + "']"));
+        element.click();
     }
 }
